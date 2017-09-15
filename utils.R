2412499b1821461771aab0bec9f6d378c2994c79
Octopus <leo_sniper@mail.ru>
2017-09-15 23:03:16 +0300

    utils

--- utils.R
+++ utils.R
@@ -1,51 +1,3 @@
-nlabel_semisizes <- lapply(seq(produce_node_attrs$label), function(i) {
-
-  t <- textGrob(produce_node_attrs$label[i],
-                x=nt$x[i],
-                y=nt$y[i],
-                default.units = "npc",
-
-                gp=gpar(fontsize=produce_node_attrs$fontsize[i] * .pt
-                ),
-                name = "text" #,
-                # vp = viewport(xscale = c(0, 1), yscale = c(0, 1))
-  )
-
-  gw <- convertWidth(grobWidth(t), "npc", TRUE) / 4
-
-  gh <- convertHeight(grobHeight(t), "npc", TRUE) / 10
-
-  c(
-    "semi_w" = gw + box_padding_x,
-    "semi_h" = gh + box_padding_y
-  )
-})
-nlabel_semisizes <- data.frame(do.call(rbind, nlabel_semisizes))
-
-elabel_semisizes <- lapply(seq(produce_edge_attrs$label), function(i) {
-
-  t <- textGrob(produce_edge_attrs$label[i],
-                x=lines_to_check$middle_x[i],
-                y=lines_to_check$middle_y[i],
-                default.units = "npc",
-
-                gp=gpar(fontsize=produce_edge_attrs$fontsize[i] * .pt
-                ),
-                name = "text"
-  )
-
-  gw <- convertWidth(grobWidth(t), "npc", TRUE) / 4
-
-  gh <- convertHeight(grobHeight(t), "npc", TRUE) / 10
-
-  c(
-    "semi_w" = gw + box_padding_x,
-    "semi_h" = gh + box_padding_y
-    )
-})
-elabel_semisizes <- data.frame(do.call(rbind, elabel_semisizes))
-
-
 get_nlabel_boxes <- function(layout1, nlabel_semisizes){
   return(data.frame("x1"=layout1[, 1] - nlabel_semisizes$semi_w,
                     "y1"=layout1[, 2] - nlabel_semisizes$semi_h,
@@ -104,37 +56,15 @@ unit_vector <- function(c1, c2){
 #' @param q1 = q2 = 1
 #' max(n_iter) = 999 # friction because
 
-# debug:
-n1 <- 1
-n2 <- 16
-e1 <- 1
-e2 <- 2
-c1 <- centroid(nlabel_boxes[1, ])
-c2 <- centroid(nlabel_boxes[2, ])
-unit_vector(c1, c2)
-force = 1e-6
-layout1 <- layout_with_kk(m)
-range01 <- function(x){(x-min(x))/(max(x)-min(x))}
-layout1 <- range01(layout1)
-# (if unit_vector[n1,1] > 1.5 * unit_vector[n1,2] u may use *semiwidth)
-# :debug
 
-# nlabel_boxes <- as.data.frame(nlabel_boxes)
-# elabel_boxes <- as.data.frame(elabel_boxes)
+# which(nt$label=="Succinate") #13
 
-# which(nt$label=="alpha−D−Glucose 6−phosphate") #53
-
-layout2 <- force_alg(layout1,
-                     nlabel_semisizes,
-                     elabel_semisizes,
-                     edges,
-                     n_iter = 500, force = 1e-5)
 
 make_giff2 <- function(layouts){
   for (i in seq_along(layouts)) {
     layout1 <- layouts[[i]]
-    png(filename=paste0("../../../Desktop/r/1010_10-5_1000/", i, ".png"), res = 144, width = 8.5, height = 11, units = "in")
-    plot(ggnet2(m, mode = as.matrix(layout1),
+    png(filename=paste0("../../../Desktop/r/1209_10-5_100_Y_rescale_paddProp_noSrawl/", i, ".png"), res = 144, width = 8.5, height = 11, units = "in")
+    plot(ggnet2(m, mode = as.matrix(layout1), layout.exp = 0.2,
                 node.size = produce_node_attrs$width, max_size=5, node.color = produce_node_attrs$color,
                 ## alpha = ifelse(produce_node_attrs$width == 0.5, 0, 1),
                 node.label = V(m)$label, label.size = produce_node_attrs$fontsize, label.color = "grey13", #label.trim = 15,
@@ -145,6 +75,13 @@ make_giff2 <- function(layouts){
 
   }
 }
+
+layout2 <- force_alg(layout1,
+                     nlabel_semisizes,
+                     elabel_semisizes,
+                     edges,
+                     n_iter = 200, force = 1e-6)
+
 make_giff2(layout2$layouts)
 
 # $ ffmpeg -f image2 -i 08_09_f5e-5_%d.png video.gif
